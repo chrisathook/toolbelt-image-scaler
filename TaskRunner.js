2f85f62b221fa01b0780dda7fdd8020eb35ffd65
Christopher Watts <chris@byhook.com>
2017-05-31 12:32:24 -0700

    handling if directory exists

--- TaskRunner.js
+++ TaskRunner.js
@@ -19,15 +19,18 @@ let run = function (jsonObj) {
     JobLoader(jsonObj).then(function (jobs) {
       console.log('!!! Jobs Loaded');
       const queue = JobQueue();
-      
-      
       queue.runJobs(jobs).then(function () {
           console.log('All Jobs Done')
         })
         .then(function () {
           resolve();
         })
-    });
+    })
+      .catch (function (value){
+        
+        console.log ('job loading failed',value)
+        
+      });
   });
 };
 let helloWorld = function (data, console) {
